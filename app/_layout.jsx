d6104ba356de5e2e20fdd9b0253c8ca31aff7716
Muhammad Windows <msalman2339@gmail.com>
2024-07-29 23:57:50 +0500

    initial setup

---
+++ app/_layout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Slot, Stack } from "expo-router";
+
+const RootLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack>
+  );
+};
+
+export default RootLayout;
---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+const App = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-white">Hu</Text>
+    </View>
+  );
+};
+
+export default App;
